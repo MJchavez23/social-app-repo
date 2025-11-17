853405535c1d3f9793b887e16a37085c3080d322
Mateo Chavez <mchavez.castillo23@gmail.com>
2025-11-17 10:09:43 -0500

    Create find by id logic.

--- user-service/src/main/java/com/example/userservice/web/controller/WebController.java
+++ user-service/src/main/java/com/example/userservice/web/controller/WebController.java
@@ -2,6 +2,7 @@ package com.example.userservice.web.controller;
 
 import com.example.userservice.domain.model.User;
 import com.example.userservice.domain.model.exceptions.EmailAlreadyExistException;
+import com.example.userservice.domain.model.exceptions.UserNotFoundException;
 import com.example.userservice.domain.model.exceptions.UsernameAlreadyExistException;
 import com.example.userservice.domain.usecases.CreateUserUseCase;
 import com.example.userservice.domain.usecases.FindUserUseCase;
@@ -9,12 +10,12 @@ import com.example.userservice.web.dto.RequestDto;
 import com.example.userservice.web.mapper.WebMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,6 +37,12 @@ public class WebController {
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = findUserUseCase.findAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable UUID id) throws UserNotFoundException {
+        User user = findUserUseCase.findById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
 }
